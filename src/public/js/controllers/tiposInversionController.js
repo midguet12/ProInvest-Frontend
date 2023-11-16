b2ed937cf75f52615bf1340dca52bc23a6112bde
midguet12 <midguet12@hotmail.com>
2023-11-16 15:54:31 -0600

    cleaning

--- src/public/js/controllers/tiposInversionController.js
+++
@@ -1,9 +0,0 @@
-export async function obtenerTiposInversion() {
-    const url='http://localhost:3000/tiposInversion';
-    const response = await fetch(url);
-    const tiposInversion = await response.json();
-
-    let mapaTiposInversion = {};
-    tiposInversion.forEach(tipo => mapaTiposInversion[tipo.nombre] = tipo);
-    return mapaTiposInversion;
-}
