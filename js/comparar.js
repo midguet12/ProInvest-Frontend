2111c5744cdaa3cb417b3f142feb1b08e9065677
Alejandro Chacón <85378121+witchdrums@users.noreply.github.com>
2023-11-18 10:26:38 -0600

    Simular
    Creo que ya quedaron las gráficas :D

---
+++ js/comparar.js
@@ -0,0 +1,108 @@
+import {obtenerTiposInversion} from "./controllers/tiposInversionController.js"
+import {grafica} from "./grafica.js"
+const tiposInversiones = document.getElementById("tipos-inversiones");
+const numberMonto = document.getElementById("monto");
+const rangePlazo = document.getElementById("plazo");
+
+let numTiposInversionesSeleccionados = 1;
+
+let mapTiposInversiones= {}
+
+window.addEventListener("load", async function(){
+    //inicializarBotonSimularInversion();
+    mapTiposInversiones = await obtenerTiposInversion();
+    llenarLista();
+    graficarTipoInversion();
+});
+
+function llenarLista(){
+    let contador = 0;
+    for (let key in mapTiposInversiones){
+        let checkTipoInversion = document.createElement('input');
+        checkTipoInversion.setAttribute("id", mapTiposInversiones[key].nombre);
+        checkTipoInversion.setAttribute("type", "checkbox");
+        checkTipoInversion.setAttribute("autocomplete", "off");
+        checkTipoInversion.classList.add("btn-check");
+        tiposInversiones.appendChild(checkTipoInversion);
+        if (contador === 0){
+            checkTipoInversion.setAttribute("CHECKED", "");
+        }
+        checkTipoInversion.addEventListener("click", ()=>{
+            manejarSeleccion(checkTipoInversion);
+            graficarTipoInversion();
+        });
+
+        let labelTipoInversion = document.createElement("label");
+        labelTipoInversion.classList.add("btn");
+        labelTipoInversion.classList.add("btn-outline-primary");
+        labelTipoInversion.setAttribute("for", mapTiposInversiones[key].nombre);
+        labelTipoInversion.innerText = mapTiposInversiones[key].nombre;
+
+        tiposInversiones.appendChild(labelTipoInversion);
+        contador++;
+    }
+}
+
+function manejarSeleccion(inputTipoInversion){
+    if (estaSeleccionado(inputTipoInversion)){
+        inputTipoInversion.removeAttribute("CHECKED");
+        numTiposInversionesSeleccionados--;
+        if (numTiposInversionesSeleccionados === 3){
+            bloquearTiposInversion(false);
+        }
+    }else{
+        inputTipoInversion.setAttribute("CHECKED","");
+        numTiposInversionesSeleccionados++;
+        if (numTiposInversionesSeleccionados > 3){
+            bloquearTiposInversion(true);
+        }
+    }
+}
+
+function graficarTipoInversion(){
+    grafica.data.datasets[0].data = [];
+    grafica.data.labels = [];
+    const seleccion =
+        Array.from(tiposInversiones.children).filter(tipo => tipo.hasAttribute("CHECKED"));
+
+    seleccion.forEach((tipo) => {
+        let nombreInversion = tipo.getAttribute("id");
+        grafica.data.labels.push(nombreInversion);
+        let plazoEnAnios = Number(document.getElementById("plazo").value);
+        let inversionFinal = Number(document.getElementById("monto").value);
+        let tasa = mapTiposInversiones[nombreInversion].tasa;
+        for(let i = 0; i<plazoEnAnios; i++){
+            inversionFinal += inversionFinal * tasa;
+        }
+        grafica.data.datasets[0].data.push(inversionFinal);
+    });
+    grafica.update();
+}
+
+function bloquearTiposInversion(bloquear){
+    for(let i = 0; i < tiposInversiones.children.length; i += 2){
+        if (!estaSeleccionado(tiposInversiones.children[i])){
+            if (bloquear){
+                tiposInversiones.children[i].setAttribute("DISABLED", "");
+            }else{
+                tiposInversiones.children[i].removeAttribute("DISABLED");
+            }
+        }
+    }
+}
+
+function estaSeleccionado(tipoInversion){
+    return tipoInversion.hasAttribute("CHECKED");
+}
+
+numberMonto.addEventListener("change", (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("monto", numberMonto.value);
+    graficarTipoInversion();
+});
+
+rangePlazo.addEventListener("change", (e) =>{
+    e.preventDefault();
+    sessionStorage.setItem("plazo", rangePlazo.value);
+    graficarTipoInversion();
+});
