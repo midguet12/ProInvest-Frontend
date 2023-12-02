37006420036b29c2b624a05372dc67b5cb58a458
Alejandro Chacón <85378121+witchdrums@users.noreply.github.com>
2023-12-02 17:37:10 -0600

    Validación en la configuración del simulador

--- js/configurar.js
+++ js/configurar.js
@@ -1,14 +1,14 @@
 import {obtenerConfiguracionSimulador,
-        reestablecerValores,
         actualizarConfiguracionSimulador} from "./controllers/configuracionSimuladorController.js";
-import {mostrarError,mostrarToast} from "./components/manejadorMensajes.js";
+import {manejarRespuesta,mostrarError,mostrarToast} from "./components/manejadorMensajes.js";
+
 
 const montoMinimo = document.getElementById("monto-minimo");
 const montoMaximo = document.getElementById("monto-maximo");
 const plazoMinimo = document.getElementById("plazo-minimo");
 const plazoMaximo = document.getElementById("plazo-maximo");
 const guardar = document.getElementById("guardar");
-const reestablecer = document.getElementById("reestablecer");
+
 
 if (sessionStorage.key("exito") !== null){
     mostrarToast(sessionStorage.getItem("exito"));
@@ -16,6 +16,17 @@ if (sessionStorage.key("exito") !== null){
 }
 
 const respuesta = await obtenerConfiguracionSimulador();
+
+function actualizarPlazos(){
+    plazoMaximo.min = parseInt(plazoMinimo.value) + 1;
+    plazoMinimo.max = parseInt(plazoMaximo.value) - 1;
+}
+
+function actualizarMontos(){
+    montoMaximo.min = parseInt(montoMinimo.value) + 10000;
+    montoMinimo.max = parseInt(montoMaximo.value) - 10000;
+}
+
 if (respuesta.ok){
     const configuracionSimuladorActual = await respuesta.json();
     plazoMinimo.disabled = false;
@@ -23,28 +34,42 @@ if (respuesta.ok){
     montoMinimo.disabled = false;
     montoMaximo.disabled = false;
     guardar.disabled = false;
-    reestablecer.disabled = false;
     plazoMinimo.value = configuracionSimuladorActual.plazoMinimo;
     plazoMaximo.value = configuracionSimuladorActual.plazoMaximo;
     montoMinimo.value = configuracionSimuladorActual.montoMinimo;
     montoMaximo.value = configuracionSimuladorActual.montoMaximo;
+    actualizarPlazos();
+    actualizarMontos();
 }else{
     mostrarError("No hay conexión.");
 }
 
+addEventListener("submit", async() =>{
 
-document.getElementById("reestablecer").addEventListener("click", (e) =>{
-    reestablecerValores();
-})
-
-addEventListener("submit", () =>{
-
-    actualizarConfiguracionSimulador({
-            plazoMinimo: plazoMinimo.value,
-            plazoMaximo: plazoMaximo.value,
-            montoMinimo: montoMinimo.value,
-            montoMaximo: montoMaximo.value,
-    });
+    const respuesta = await actualizarConfiguracionSimulador({
+        plazoMinimo: plazoMinimo.value,
+        plazoMaximo: plazoMaximo.value,
+        montoMinimo: montoMinimo.value,
+        montoMaximo: montoMaximo.value,
+    })
+    manejarRespuesta(respuesta);
     sessionStorage.setItem("exito","Registro exitoso.");
 
+})
+
+$("input[type='number']").inputSpinner({buttonsOnly: true });
+plazoMinimo.addEventListener("change", (e) => {
+    actualizarPlazos();
+})
+
+plazoMaximo.addEventListener("change", (e) => {
+    actualizarPlazos()
+})
+
+montoMinimo.addEventListener("change", (e) => {
+    actualizarMontos();
+})
+
+montoMaximo.addEventListener("change", (e) => {
+    actualizarMontos();
 })
