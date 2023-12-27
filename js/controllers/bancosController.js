d96b2c5682bb244e9e970dd058d6c45d4bb18745
Alejandro Chacón <85378121+witchdrums@users.noreply.github.com>
2023-12-27 15:27:59 -0600

    Bank editing use case
    CU-13 is done.

--- js/controllers/bancosController.js
+++ js/controllers/bancosController.js
@@ -9,3 +9,26 @@ export async function registrarBancos(nuevoBanco){
     };
     return await fetch(url, peticion);
 }
+
+export async function actualizarBancos(nuevoBanco){
+    const url='http://localhost:3000/bancos';
+    const peticion = {
+        method: "PUT",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify(nuevoBanco)
+    };
+    return await fetch(url, peticion);
+}
+
+export async function obtenerBancos(){
+    const url='http://localhost:3000/bancos';
+    const peticion = {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json",
+        },
+    };
+    return await fetch(url, peticion);
+}
