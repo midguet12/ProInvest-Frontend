e38bc4449d2475b63fe0e78a6cd0013a96c11623
midguet12 <midguet12@hotmail.com>
2023-12-27 03:29:39 -0500

    login
    terminado, pruebas aprobadas

--- login/iniciarsesion.js
+++ login/iniciarsesion.js
@@ -3,16 +3,25 @@ const contrasenaInput = document.getElementById('contrasenaInput');
 const iniciarSesionButton = document.getElementById("iniciarSesionButton");
 const mensajeroP = document.getElementById('mensajero');
 
-let prueba;
-
-//window.localStorage.setItem("key", "token");
-
-
 iniciarSesionButton.onclick = async function(){
     const contrasenaCifrada = await hash( correoElectronicoInput.value + contrasenaInput.value);
 
-    iniciarSesion(correoElectronicoInput.value, contrasenaCifrada);
+    const expresionRegular = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
+    let validacion = correoElectronicoInput.value.match(expresionRegular);
+
+    if (validacion!= null) {
+        iniciarSesion(correoElectronicoInput.value, contrasenaCifrada);
+    } else {
+        console.log("No es correo");
+        correoElectronicoInput.style.borderColor = 'red';
+        mensajeroP.innerHTML = "Por favor ingresa un correo valido";
+
+        setTimeout(()=>{
+            correoElectronicoInput.style.borderColor = 'black';
+            mensajeroP.innerHTML = " ";
+        },1000 * 2);
+    }
 }
 
 async function hash(input){
@@ -26,7 +35,9 @@ async function hash(input){
 }
 
 async function iniciarSesion(correoElectronico, contrasenaCifrada){
-    //let respuesta = new XMLHttpRequest();
+
+   
+
 
 
     const body = {
@@ -47,9 +58,6 @@ async function iniciarSesion(correoElectronico, contrasenaCifrada){
         referrerPolicy: "no-referrer", 
         body: JSON.stringify(body)
     });
-
-    
-    
 
     if (respuesta.status == 201) {  
         let redirectUrl = "profile/index.html";
@@ -89,48 +97,4 @@ async function iniciarSesion(correoElectronico, contrasenaCifrada){
         console.log(`Error: ${respuesta.status}`);
     }
 
-
- 
-    
-
-   /*
-
-    xhr.onreadystatechange = function () {
-        if (xhr.status == 201 &&  xhr.readyState == 4) {  
-            let redirectUrl = "profile/index.html";
-
-            let responseJSON = JSON.parse(xhr.responseText);
-            let token = responseJSON.token;
-            let correoElectronico = responseJSON.correoElectronico;
-
-            if (token != null) {
-                //window.localStorage.clear();
-                window.localStorage.setItem("token", token);
-                window.localStorage.setItem("correoElectronico", correoElectronico);
-                window.location.href = redirectUrl;
-
-            } 
-        } else if(xhr.status == 404 && xhr.readyState == 4){
-          console.log("Correo no existe");
-          correoElectronicoInput.style.borderColor = 'red';
-          mensajeroP.innerHTML = "Correo no se encuentra registrado";
-
-            setTimeout(()=>{
-                correoElectronicoInput.style.borderColor = 'black';
-                mensajeroP.innerHTML = " ";
-            },1000 * 2);
-          
-        } else if (xhr.status == 401 && xhr.readyState == 4) {
-            console.log("Contraseña incorrecta");
-            contrasenaInput.style.borderColor = 'red';
-            mensajeroP.innerHTML = "Contraseña incorrecta"
-
-            setTimeout(()=>{
-                contrasenaInput.style.borderColor = 'black';
-                mensajeroP.innerHTML = " ";
-            },1000 * 2);
-        } else {
-            console.log(`Error: ${xhr.status}`);
-        }
-    };*/
 }
--- login/profile/profile.js
+++ login/profile/profile.js
@@ -5,20 +5,10 @@ const fechaNacimientoP = document.getElementById("fecha-nacimiento");
 const celularP = document.getElementById("celular");
 const tituloH3 = document.getElementById("titulo");
 
-//const correoElectronico = window.localStorage.getItem("correoElectronico");
-const correoElectronico = "midguet12@hotmail.com";
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb3JyZW9FbGVjdHJvbmljb1Npc3RlbWEiOiJtaWRndWV0MTJAaG90bWFpbC5jb20iLCJjb250cmFzZW5hU2lzdGVtYSI6IjMzNWI5MDBmNTRlZjAyN2MzOTAzZDFmNWQ1M2YyMDUyMjA3YThiNDcxYTMzNzM3OWI2Njg5MDZiMjkyMjZkMmUiLCJleHAiOjE3MDM2NjA0MjYyOTYsImlhdCI6MTcwMzY1NjgyNn0.SnbSGOoRnav_e5_UJpTFBPcWOMgxrfMfutFIqpVHyO0";
-//const token = window.localStorage.getItem("token");
+const correoElectronico = window.localStorage.getItem("correoElectronico");
+const token = window.localStorage.getItem("token");
 
 obtenerUsuario(correoElectronico, token);
-
-
-
-
-/*correoElectronicoP.innerHTML += " " + correoElectronico;
-nombresP.innerHTML += " " + respuestaJSON.nombres;
-apellidosP.innerHTML += " " + respuesta.apellidoPaterno + " " + respuesta.apellidoMaterno;*/
-
 
 async function obtenerUsuario(correoElectronico, token){
     const urlServidor = "http://localhost:3000/obtenergerente";
@@ -46,8 +36,5 @@ async function obtenerUsuario(correoElectronico, token){
     nombresP.innerHTML += " " + respuestaJSON.nombres;
     apellidosP.innerHTML += " " + respuestaJSON.apellidoPaterno + " " + respuestaJSON.apellidoMaterno;
     fechaNacimientoP.innerHTML += " " + respuestaJSON.fechaNacimiento;
-
-
-
     celularP.innerHTML += " " + respuestaJSON.celular;
 }
